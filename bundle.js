8c84f96091f22f02089d027bfb0ce0bd5f61c99a
Jeongmin Woo <greenblues1190@gmail.com>
2022-04-09 18:13:47 +0900

    Updates

--- bundle.js
+++ bundle.js
@@ -417,6 +417,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _domains_Auth__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../domains/Auth */ "./src/domains/Auth.ts");
 /* harmony import */ var _configs_constants__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../configs/constants */ "./src/configs/constants.ts");
 /* harmony import */ var _utils_commons__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ../utils/commons */ "./src/utils/commons.ts");
+/* harmony import */ var _configs_api__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ../configs/api */ "./src/configs/api.ts");
 
 
 
@@ -426,6 +427,7 @@ __webpack_require__.r(__webpack_exports__);
 function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = (0,_babel_runtime_helpers_getPrototypeOf__WEBPACK_IMPORTED_MODULE_4__["default"])(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = (0,_babel_runtime_helpers_getPrototypeOf__WEBPACK_IMPORTED_MODULE_4__["default"])(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return (0,_babel_runtime_helpers_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_3__["default"])(this, result); }; }
 
 function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
 
 
 
@@ -470,7 +472,7 @@ var Router = /*#__PURE__*/function (_Component) {
     value: function getComponent() {
       var location = _domains_Browser__WEBPACK_IMPORTED_MODULE_6__.browser.useStore(function (state) {
         return state.location;
-      });
+      }).slice(_configs_api__WEBPACK_IMPORTED_MODULE_10__.BASE.length);
       var routes = this.state.routes;
       var currentRoute = routes.filter(function (route) {
         return route.path.split('|').includes(location) || route.path === _configs_constants__WEBPACK_IMPORTED_MODULE_8__.PAGES.DEFAULT.PATH;
@@ -3799,9 +3801,11 @@ module.exports = styleTagTransform;
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "SERVER": () => (/* binding */ SERVER)
+/* harmony export */   "SERVER": () => (/* binding */ SERVER),
+/* harmony export */   "BASE": () => (/* binding */ BASE)
 /* harmony export */ });
 var SERVER = 'https://vendingmachin-json-server-auth.herokuapp.com';
+var BASE = '/javascript-vendingmachine';
 
 
 /***/ }),
--- configs/api.d.ts
+++ configs/api.d.ts
@@ -1,1 +1,2 @@
 export declare const SERVER: "https://vendingmachin-json-server-auth.herokuapp.com";
+export declare const BASE: "/javascript-vendingmachine";
